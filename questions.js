6170cc3f621efe48bea2881dfcc365d2ce552615
Your Name <you@example.com>
2019-04-18 21:04:00 +0200

    DataStructure und erster sketch vom JS

---
+++ questions.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function()
+{
+  var frage=$('.frage')
+  var score=900;
+  var richtig=1;
+  var start = new Date().getTime();
+
+      frage.click(function()
+      {
+        var stop = new Date().getTime();
+          if(this.id==richtig)
+          {
+            timer(stop);
+          }
+          else
+          {
+            score=0;
+          }
+      });
+      function timer(stop)
+      {
+        //console.log(score-(((stop-start)/10)*(score/1500)));
+        var complete=stop-start;
+        if(complete<15000)
+        score=score-(((complete)/10)*(score/1500));
+        else
+        score=0;
+      }
+
+});
