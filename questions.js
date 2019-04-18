86213e00b72396b0916e9888bdead6cddfee735e
Your Name <you@example.com>
2019-04-18 22:35:44 +0200

    Halbwegs funktionierendes frageSystem

--- questions.js
+++ questions.js
@@ -1,31 +1,40 @@
 
+
 $(document).ready(function()
 {
   var antwort=$('.antwort')
   var score=900;
   var richtig=1;
   var start = new Date().getTime();
-
       antwort.click(function()
       {
         var stop = new Date().getTime();
+        var end=stop-start;
           if(this.id==richtig)
           {
-            timer(stop);
+            timer(end);
+            window.location = "transition.html"+'#'+end+'s'+score;
+                //füge Transition auf seite zwischen Fragen zu, speicher in Datenbank
           }
           else
           {
             score=0;
+            window.location = "transition.html" + '#' + end +'s'+0;  //füge Transition auf seite zwischen Fragen zu, speicher in Datenbank
           }
       });
-      function timer(stop)
-      {
-        //console.log(score-(((stop-start)/10)*(score/1500)));
-        var complete=stop-start;
-        if(complete<15000)
-        score=score-(((complete)/10)*(score/1500));
-        else
-        score=0;
-      }
+
+      setTimeout(function timeOver()
+    {
+      score=0;
+      console.log("Antwortszeit überschritten");
+      window.location = "transition.html"+'#'+15000+'s'+0;},15000)  //füge Transition auf seite zwischen Fragen zu, speicher in DB
+    function timer(end)
+    {
+      //console.log(score-(((stop-start)/10)*(score/1500)));
+      if(end<15000)
+      score=score-((end/10)*(score/1500));
+      else
+      score=0;
+    }
 
 });
---
+++ transition.js
@@ -0,0 +1,24 @@
+$(document).ready(function()
+{
+var time = window.location.hash.substring(1).split('s')[0];
+var score= window.location.hash.substring(1).split('s')[1];
+append();
+function append()
+{
+  console.log(time);
+  console.log(score);
+if(time==15000)
+{
+$('#Antwortszeit').append("Bist du ein Denker ? Deine zeit ist abgelaufen Aristotheles");
+}
+else if(score==0&&time!=15000)
+{
+$('#Punkte').append("Ouch das muss weh tun so scheiße zu sein.");
+}
+else if(score!=0&&time!=15000)
+{
+$('#Antwortszeit').append("Du hast in "+parseInt(time/100)/10+" s geantwortet.");
+$('#Punkte').append("Du hast "+parseInt(score)+" Punkte.");
+}
+}
+});
