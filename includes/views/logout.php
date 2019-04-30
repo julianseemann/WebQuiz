15bc3e8ddcce36fdd84fe7b94d0c1e562a8a66af
seemanjulian <1810653188@stud.fh-kufstein.ac.at>
2019-04-30 09:00:12 +0200

    logoutstyle

--- includes/views/logout.php
+++ includes/views/logout.php
@@ -19,9 +19,9 @@ echo $this->header;
 -->
 <body>
     <div id="main">
-        <div class="row">
-            <h1 class="col-xs-12">Logout erfolgreich</h1>
-            <p class="col-xs-12">Sie sind jetzt abgemeldet. <a href="login">Klicken Sie hier um sich wieder anzumelden.</a></p>
+        <div class="rowlogout">
+            <h1 class="rowlogouttext1">Logout erfolgreich</h1>
+            <p class="rowlogouttext2">Sie sind jetzt abgemeldet. <a href="login">Klicken Sie hier um sich wieder anzumelden.</a></p>
         </div>
     </div>
 </body>
