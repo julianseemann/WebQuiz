2fa86bdf5a01e01c8b58e33927d05d3f932c7b76
seemanjulian <1810653188@stud.fh-kufstein.ac.at>
2019-04-30 09:45:02 +0200

    logoutstyletextmitte

--- includes/views/logout.php
+++ includes/views/logout.php
@@ -3,27 +3,19 @@
 echo $this->header;
 
 ?>
-<!-- <nav>
-<ul class="navbar-nav">
-  <li class="nav-item">
-    <a class="nav-link" href="scoreboard">Scoreboard</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="quizlist">Quiz List</a>
-  </li>
-  <li class="nav-item active">
-    <a class="nav-link" href="logout">logout</a>
-  </li>
-</ul>
-</nav>
--->
+
+
 <body>
-    <div id="main">
-        <div class="rowlogout">
-            <h1 class="rowlogouttext1">Logout erfolgreich</h1>
+
+
+
+
+        <div class="rowlogout text-center">
+
+             <h1 class="rowlogouttext1">Logout erfolgreich</h1>
             <p class="rowlogouttext2">Sie sind jetzt abgemeldet. <a href="login">Klicken Sie hier um sich wieder anzumelden.</a></p>
+
         </div>
-    </div>
 </body>
 
 <?php
